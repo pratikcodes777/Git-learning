4a03484510ff30478d1342e43b99a0786047d719
Pratik Barakoti <pratik.barakoti01@gmail.com>
2024-06-04 16:51:07 +0545

    sub functionality

--- calc.py
+++ calc.py
@@ -10,3 +10,20 @@ def multiply(x,y):
 
 mul = multiply(a,b)
 print(mul)
+
+def divide(x,y):
+    return x/y
+
+
+div = divide(a,b)
+print(div)
+
+
+
+
+def subtract(x,y):
+    return x-y
+
+
+sub = subtract(a,b)
+print(sub)
